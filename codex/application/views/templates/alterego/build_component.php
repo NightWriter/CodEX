a7c20896d4532424ee380da9c67f2845a4dfff38
Ivan Mashkantsev <depechemodefan@alterego.biz.ua>
2012-04-10 15:52:44 +0300

    closes #19

--- codex/application/views/templates/alterego/build_component.php
+++ codex/application/views/templates/alterego/build_component.php
@@ -16,7 +16,7 @@ jQuery(document).ready(function(){
     
     jQuery('#add_field').click(function(){
         
-        jQuery('#area_fields .group-new:last-child').after('<div class="group-new">'+jQuery('#blanck_fields').html()+'</div>');
+        jQuery('#area_fields .group-new:last-child').after('<hr><div class="group-new">'+jQuery('#blanck_fields').html()+'</div>');
         return false;
     });
     
